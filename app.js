100e4980b0dc6c5242910c917c24de4e6862849b
audwo <kmjstj3@naver.com>
2023-11-21 16:31:36 +0900

    patch Error

--- app.js
+++ app.js
@@ -120,9 +120,11 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+  console.error(err);
   res.loacls.message = err.message;
   res.locals.erorr = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
