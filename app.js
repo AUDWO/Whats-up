c7efbbe17f2e8a3e47b1a398228523edbe24c34e
audwo <kmjstj3@naver.com>
2023-11-21 19:19:04 +0900

    patch

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, "prototype-client/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "prototype-client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/prototype-client/build/index.html"));
 });
 
 app.use("/page", pageRouter);
